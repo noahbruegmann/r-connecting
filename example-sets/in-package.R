92efbd8e29edc8b438325d5737a22d76f2d40de9
Noah Bruegmann <bruegm@gmail.com>
2015-08-28 14:06:39 -0700

    Starts to compile ideas for example datasets available through R packages.

---
+++ example-sets/in-package.R
@@ -0,0 +1,13 @@
+## Here are some examples of datasets that you can
+ # access through packages if you'd like to side-step
+ # setting up other data connections and start playing
+ # with R right away.
+
+library(MASS)
+summary(Boston)
+?Boston
+
+library(ISLR)
+summary(Auto)
+summary(Smarket)
+?College
